beb9de144a5fef8e071daf69d49c9a6288aba496
JoeJoeTV <hannes1033@gmail.com>
2021-05-01 23:35:18 +0200

    + Change Color depending on Focused state + Only capture key combo once until the control is clicked again + Expose Align and Anchors properties. - Remove Caret

--- visual/HotKey.pas
+++ visual/HotKey.pas
@@ -38,6 +38,11 @@ type
     FNoModifier: Boolean;
     FOnChange: TNotifyEvent;
 
+    FBackgroundColor: TColor;
+    FBorderColor: TColor;  
+    FTextColor: TColor;
+    FAcceptsInput: Boolean;
+
     function GetCharFromVirtualKey(Key: Word): String;
   protected
 {    procedure CreateParams(var Params: TCreateParams); override;}
@@ -53,13 +58,16 @@ type
     constructor Create(AOwner: TComponent); override;
     procedure Paint; override;
     procedure EditingDone; override;
-  published
-    property BorderStyle stored false;
-    property Cursor stored false;
+  published        
+    property Align;
+    property Anchors;
+    property Font;
+    property BorderStyle stored false; //Has no effect
+    property Cursor stored false; //Has no effect
     property Left;
     property Top;
-    property Width;
-    property Height;
+    property Width default 80;
+    property Height default 27;
     property TabOrder;
     property TabStop stored false default true;
     property AutoSize;
@@ -82,9 +90,12 @@ constructor THotKey.Create(AOwner: TComponent);
 begin
   inherited Create(AOwner);
   ControlStyle := ControlStyle + [csCaptureMouse, csClickEvents, csRequiresKeyboardInput];
-  BorderStyle := bsSingle;
+  BorderStyle := bsNone;
   FCursor := crIBeam;
   FNoModifier := False;
+  FBackgroundColor := clWindow;
+  FTextColor := clCaptionText;
+  FAcceptsInput := False;
   {FAutoSelect := True;
   FAutoSelected := False;
   FTextChangedByRealSetText := False;
@@ -109,25 +120,42 @@ var
 begin
   inherited;
   DebugLn('THotKey.DoEnter');
-  CreateCaret(Handle, 0, 1, 16);
-  GetCaretPos(Point);
-  DebugLn(', X: ' + IntToStr(Point.x));
-  SetCaretPos(1, Point.y);
-  ShowCaret(Handle);
+  //CreateCaret(Handle, 0, 1, 16);
+  //GetCaretPos(Point);
+  //DebugLn(', X: ' + IntToStr(Point.x));
+  //SetCaretPos(1, Point.y);
+  //ShowCaret(Handle);
+
+  FBackgroundColor := clWindow;
+  FBorderColor := clHighlight;
+
+  FTextColor := clHighlight;
+  FAcceptsInput := True;
   Invalidate;
 end;
 
 procedure THotKey.DoExit;
 begin
   DebugLn('THotKey.DoExit');
-  HideCaret(Handle);
+  //HideCaret(Handle);
   {DestroyCaret(Handle);}
+  FBackgroundColor := clWindow;
+  FBorderColor := clBtnShadow;  
+  FTextColor := clCaptionText;
+  FAcceptsInput := False;
   inherited;
 end;
 
 procedure THotKey.MouseDown(Button: TMouseButton; Shift:TShiftState; X,Y:Integer);
 begin
   inherited;
+  
+  if Focused then
+    begin           
+      FTextColor := clHighlight;
+      FAcceptsInput := True;
+      Refresh;
+    end;
 
   if CanSetFocus then begin
     SetFocus;
@@ -148,36 +176,59 @@ begin
   inherited;
   DebugLn('THotKey.KeyUp ' + IntToStr(Key));
 
-  filteredShiftState := Shift * [ssShift, ssAlt, ssCtrl, ssMeta];
-  if ((Key = 8) or (Key = 46)) and (filteredShiftState = []) then
-  begin
-     Hotkey := 0;
-  end else
-  begin
-    if not(FNoModifier) and (filteredShiftState = []) then
-      filteredShiftState := [ssCtrl];
+  if FAcceptsInput then
+    begin
+      filteredShiftState := Shift * [ssShift, ssAlt, ssCtrl, ssMeta];
+      if ((Key = 8) or (Key = 46)) and (filteredShiftState = []) then
+      begin
+        Hotkey := 0;
+      end else
+      begin
+        if not(FNoModifier) and (filteredShiftState = []) then
+          filteredShiftState := [ssCtrl];
 
-    newShortCut := ShortCut(Key, filteredShiftState);
-    if (ShortCutToText(newShortCut) <> '') then
-      Hotkey := newShortCut;
+        newShortCut := ShortCut(Key, filteredShiftState);
+        if (ShortCutToText(newShortCut) <> '') then
+          Hotkey := newShortCut;
 
-    if Assigned(FOnChange) then
-      FOnChange(Self);
-  end;
+        if Assigned(FOnChange) then
+          FOnChange(Self);
+      end;
+                                
+      FTextColor := clCaptionText;
+      FAcceptsInput := False;
+    end;
+
   Invalidate;
 end;
 
 procedure THotKey.Paint;
+const
+  BorderWidth = 1;
+  LeftRightSpacing = 5;
 var
   txt: string;
+  textBox: TRect;
 begin
   Inherited;
-  Canvas.Brush.Color := clWhite;
+
+  //Draw Background
+  Canvas.Brush.Color := FBackgroundColor;
   Canvas.Font.Assign(Self.Font);
-  Canvas.FillRect(ClientRect);
+  Canvas.Pen.Color := clRed;
+
+  //Draw Border Rectangle
+  Canvas.Pen.Color := FBorderColor;
+  Canvas.Pen.Width := 1;
+  Canvas.Rectangle(ClientRect);
+
   //Canvas.Brush.Assign(Self.Brush);  No Default Brush!
   if Hotkey <> 0 then txt := ShortCutToText(Hotkey) else txt := 'None';
-  Canvas.TextOut(1, 1, txt);
+
+  //Draw Text
+  Canvas.Font.Color := FTextColor;
+  textBox := TRect.Create(BorderWidth+LeftRightSpacing,BorderWidth,ClientRect.Width-(BorderWidth+LeftRightSpacing)*2, ClientRect.Height-2*BorderWidth);
+  Canvas.TextRect(textBox, textBox.Left, ((textBox.Height) div 2 - Canvas.TextHeight(txt) div 2)+1, txt);
 end;
 
 procedure THotKey.EditingDone;
@@ -195,7 +246,7 @@ var
 begin
   Result := '';
   if Key <> 0 then
-  begin      
+  begin
     Shortcut := KeyToShortCut(Key, []);
     Result := ShortCutToText(Shortcut);
   end;
